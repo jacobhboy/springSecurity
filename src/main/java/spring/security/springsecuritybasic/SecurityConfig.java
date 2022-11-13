0d69b0cc7df16c4fde1b0eb7debb4fb88ffee7e7
leechangbo <jacobhboy66@gmail.com>
2022-11-13 23:37:33 +0900

    11/13/2022 SpringSecurity Study

--- src/main/java/spring/security/springsecuritybasic/SecurityConfig.java
+++ src/main/java/spring/security/springsecuritybasic/SecurityConfig.java
@@ -1,13 +1,17 @@
 package spring.security.springsecuritybasic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +21,14 @@ import java.io.IOException;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -47,5 +59,42 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         response.sendRedirect("/login");
                     }
                 });
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        request.getSession().invalidate();
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException{
+                        response.sendRedirect("/login");
+                    }
+                })
+                .deleteCookies("remember-me");
+
+        http
+                .rememberMe()
+                .rememberMeParameter("remember") // default는 remember-me //<input> name 바꾸기
+                .tokenValiditySeconds(3600)
+                .alwaysRemember(true) // 항상 리멤버미 체크 default는 false
+                .userDetailsService(userDetailsService); //remember-me기능을 사용할때 시스템에 있는 사용자계정 조회할때 필요함 필수!
+
+        //동시 세션 제어
+        http
+                .sessionManagement()
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(true);
+
+        //세션 고정 보호
+        http
+                .sessionManagement()
+                .sessionFixation().changeSessionId(); // 인증할때마다 세션 아이디 바꾸기
+
+
     }
 }
